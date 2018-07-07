ed1bdf84bacdd78339e7f4f52f038a85e79b5072
Angela Duran <duranangela19@gmail.com>
2018-07-06 21:30:03 -0600

    Update links and test for job/index

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -5,7 +5,8 @@ describe "User sees a specific job" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
 
-    visit company_job_path(company, job)
+    visit company_path(company)
+    click_link(job.title)
 
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
