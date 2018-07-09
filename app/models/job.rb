58aa99580efa49fa1ea8305937fc41315728a052
ClaireBvs <clairembeauvais@gmail.com>
2018-07-09 08:04:25 -0600

    add order for count level of interest dashboard

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,7 +5,7 @@ class Job < ApplicationRecord
   has_many :comments
 
   def self.sort_by_level_of_interest
-    group(:level_of_interest).count
+    group(:level_of_interest).order('level_of_interest DESC').count
   end
 
 end
